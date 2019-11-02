c3f37ab6e49bd8403a5aadd6ce5d8c4c29e454c9
Ruslan198209 <ruslan19820905@gmail.com>
2019-11-02 20:15:00 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("саламатсыз");
+
+
+
+
+
+
+        String pervoeSlovo = "Привет,";
+        String vtoroeSlovo = "мир!";
+        System.out.println(pervoeSlovo+vtoroeSlovo);
+        System.out.println("Как вас зовут?");
+    boolean isRuslan = true;
+        if (isRuslan) {
+            System.out.println("Привет, Руслан!");
+
+        }
+
+
+    }
+}
